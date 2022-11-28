e6156f96c2bf0078cb98bcf0cd89112b2926245d
Neo Abric <abricneo@gmail.com>
2022-11-28 22:03:02 +0100

    déclaration du tableau de données test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var str = "http://127.0.0.1:5500/produit.html?name=Nike-Air-Max-SKY&price=150€&description=Chaussure de course";
+/*var str = "http://127.0.0.1:5500/produit.html?name=Nike-Air-Max-SKY&price=150€&description=Chaussure de course";
 
 var url = new URL(str);
 
@@ -13,4 +13,55 @@ if(search_params.has('description' )){
     var description = search_params.get('description');
     document.getElementById('test').innerHTML = "<p>"+ description +"</p>";
 } 
+*/
+
+
+
+
+
+//========Déclaration du tableau de données=========
+
+const response = [
+    {
+        nomProduit:"Produit1",
+        //description:"",
+        prix:"150"
+    },
+
+    {
+        nomProduit:"Produit2",
+        //description:"",
+        prix:"1500"
+    },
+
+    {
+        nomProduit:"Produit3",
+        //description:"",
+        prix:"150"
+    },
+
+    {
+        nomProduit:"Produit4",
+        //description:"",
+        prix:"150"
+    },
+
+    {
+        nomProduit:"Produit5",
+        //description:"",
+        prix:"150"
+    },
+
+    {
+        nomProduit:"Produit6",
+        //description:"",
+        prix:"150"
+    },
+
+    {
+        nomProduit:"Produit7",
+        //description:"",
+        prix:"150"
+    },
+]
 
