7454ddd97618fd043d836170db1aaafcb7255b74
Neo Abric <abricneo@gmail.com>
2022-11-28 22:30:09 +0100

    Mise en place de toutes les données sur la page d'accueil

--- script.js
+++ script.js
@@ -65,3 +65,56 @@ const response = [
     },
 ]
 
+
+//------------Mettre les données sur la page d'accueil------------
+
+//déclaration de variable
+let nomProduit = [];
+let description = [];
+let  prix = [];
+let structureProduits = "";
+let i = [];
+
+// fonction qui va afficher les produits dans la page web auto
+function affichageProduits(response){
+
+    //select-ion élement du DOM
+    const positionElement = document.querySelector(".product-row");
+
+    //la boucle pour afficher tous les objets dans la page web
+    for (i=0; i < response.length; i++){
+
+    //mettre les données dans  les variables
+    response.forEach((element, i) =>{
+        nomProduit[i] = element.nomProduit;
+        description[i] = element.description;
+        prix[i] = element.prix;
+    });
+
+    //afficher tous les produits sur la page web
+    structureProduits += `
+            <div class="product-box">
+                <a href="http://127.0.0.1:5500/produit.html?name=Nike-Air-Max-SKY&price=150€&description=Chaussure-de-course">
+                    <div id="product-image-1"></div>
+                    <div class="product-info">
+                        <div>
+                            <h4><span>${nomProduit[i]}</span>Nike Air Max SKY</h4>
+                            <p><span>${prix[i]}</span>150€</p>
+                        </div>
+                        <span class="description-color"><p><span>${description[i]}</span>150€</p>Chaussure de course</p></span>
+                    </div>
+                </a>
+            </div>
+        `;
+        //injection html
+        positionElement.innerHTML = structureProduits;
+
+    }
+
+
+}
+
+
+
+affichageProduits(response)
+
